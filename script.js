498fb637e1ea0876e79c893b377b8686a550654b
JaxWolf4 <71346572+JaxWolf4@users.noreply.github.com>
2020-09-21 17:42:55 -0400

    Add files via upload

---
+++ script.js
@@ -0,0 +1,7 @@
+var boxEle = document.body.querySelector(".box");
+
+var numberOne = Number(prompt("What is the first number you want to add?"));
+
+var numberTwo = Number(prompt("What is the second number you want to add?"));
+
+boxEle.innerHTML=(numberOne+numberTwo);
